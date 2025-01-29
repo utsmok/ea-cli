af95407cf02f071d7d022f8bd7f5d44d97cd5fa2
Samuel Mok <s.mok@utwente.nl>
2025-01-29 17:18:42 +0100

    additional excel file styling, e.g. for column width and url truncation

--- easy_access_cli.py
+++ easy_access_cli.py
@@ -988,6 +988,11 @@ class EasyAccessTool:
 
         Adds the sheet to the workbook and saves it, doesnt return any data.
         """
+        from openpyxl.worksheet.filters import (
+            FilterColumn,
+            Filters,
+        )
+        from openpyxl.styles import Alignment, NamedStyle
 
         @dataclass
         class ColInfo:
@@ -1001,6 +1006,8 @@ class EasyAccessTool:
             is_editable: bool = False # if True, this col can be edited
             new_name: str = "" # if not empty, this col will be renamed to this name
             default_val: str = "" # if 'is_new' is True, use this as the default value for the new col
+            max_width: int = 8 # the max length of any value present in this col, to be set while processing. Min width is this initial number.
+            count_max_width_over_40: int = 0 # the number of items in this col that are longer than 40 chars, to be set while processing
             @property
             def has_dropdown(self) -> bool:
                 return len(self.dropdown_options) > 0
@@ -1018,6 +1025,7 @@ class EasyAccessTool:
             sheet: openpyxl.worksheet.worksheet.Worksheet = field(init=False)
             file_path: str
             max_row: int = 0
+            word_wrap_style: Alignment = NamedStyle(name='wordwrap', alignment=Alignment(wrapText=True))
 
             def __post_init__(self):
                 self.sheet = wb.create_sheet(self.sheet_name, index=1)
@@ -1025,7 +1033,7 @@ class EasyAccessTool:
             def add_data(self, data: pl.DataFrame) -> None:
                 self.max_row = data.shape[0]
                 colnum = 0
-                prefix = ''
+
                 for col in self.cols:
                     colnum += 1
                     if col.new_name:
@@ -1045,9 +1053,30 @@ class EasyAccessTool:
 
                     self.sheet.cell(1, colnum).value = col_name
                     for row, cell_data in enumerate(col_data, start=2):
-                        self.sheet.cell(row, colnum).value = cell_data
+                        if not cell_data:
+                            self.sheet.cell(row,colnum).value = cell_data
+                            continue
+
+
                         if col.is_url:
+                            if '/' not in cell_data:
+                                self.sheet.cell(row,colnum).value = cell_data
+                            else:
+                                self.sheet.cell(row,colnum).value = '.../'+cell_data.split('/')[-1]
                             self.sheet.cell(row, colnum).hyperlink = cell_data
+                            if len(self.sheet.cell(row,colnum).value) > col.max_width:
+                                col.max_width = len(self.sheet.cell(row,colnum).value)
+                            if len(self.sheet.cell(row,colnum).value) > 40:
+                                col.count_max_width_over_40 += 1
+
+                        else:
+                            self.sheet.cell(row, colnum).value = cell_data
+                            if len(cell_data) > col.max_width:
+                                col.max_width = len(cell_data)
+                            if len(cell_data) > 40:
+                                col.count_max_width_over_40 += 1
+
+
 
                 for colnum, col in enumerate(self.cols):
                     col_letter = chr(ord('A') + colnum)
@@ -1065,15 +1094,31 @@ class EasyAccessTool:
                             dv.add(f"{col_letter}2")
                         else:
                             dv.add(f"{col_letter}2:{col_letter}{self.max_row+1}")
+                    if col.max_width > 40 and ((col.count_max_width_over_40 > 5) or (col.count_max_width_over_40 > self.max_row-2)):
+                        # Too much long items: cap width to 40 & enable word wrap for this col
+                        for row in range(2, self.max_row+1):
+                            self.sheet.cell(row, colnum).style = self.word_wrap_style
+                        self.sheet.column_dimensions[col_letter].bestFit = False
+                        self.sheet.column_dimensions[col_letter].width = 40
+                    else:
+                        # Acceptable width, don't enable word wrap but fit width to contents
+                        self.sheet.column_dimensions[col_letter].width = col.max_width
 
+                info(f'Added data to {self.sheet_name} in file {self.file_path}.')
+                self.create_table()
+                self.save()
             def create_table(self) -> None:
                 max_col_letter = chr(ord('A') + len(self.cols)-1)
-
                 table = ExcelTable(displayName=self.sheet_name.replace(' ',''), ref=f"A1:{max_col_letter}{self.max_row+1}")
                 table.tableStyleInfo = self.table_style
                 self.sheet.add_table(table)
+                info(f'Created table with {self.max_row} rows and {len(self.cols)} cols in sheet {self.sheet_name} of file {self.file_path}')
+
+
+
+            def save(self) -> None:
                 self.workbook.save(filename=self.file_path)
-                info(f'Added data entry sheet with {self.max_row} rows to {self.file_path}')
+                info(f'Saved .xlsx file with DataEntrySheet to {self.file_path}')
 
 
         wb = openpyxl.load_workbook(filename=str(file.path))
@@ -1114,7 +1159,6 @@ class EasyAccessTool:
         )
 
         sheet.add_data(data)
-        sheet.create_table()
 
     def create_all_items_sheet(self) -> None:
         """
